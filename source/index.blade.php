7e9042949a77bbddf9c26fe4ab208e8c049c89bc
YutoSeta <adam.biz.2016@gmail.com>
2021-11-21 14:07:16 +0900

    タブコンポーネント修正

--- source/index.blade.php
+++ source/index.blade.php
@@ -21,23 +21,59 @@
         </section>
         <section class="p-8 space-y-4">
             <h2 class="text-xl font-bold">■Alpine Example</h2>
-            <div class="w-full flex" x-data="{ 'active' : 0 }">
-                <div class="flex-auto py-2 flex justify-center items-center"
-                     x-bind:class="{ 'bg-black text-white' : active==0, 'bg-gray-100 text-black' : active!==0 }"
-                     x-on:click="active=0">タブ１
+            <div x-data="{ 'active' : 0 }">
+                <div class="w-full flex">
+                    <div class="flex-auto py-2 flex justify-center items-center"
+                         x-bind:class="{ 'bg-black text-white' : active==0, 'bg-gray-100 text-black' : active!==0 }"
+                         x-on:click="active=0">タブ１
+                    </div>
+                    <div class="flex-auto py-2 flex justify-center items-center"
+                         x-bind:class="{ 'bg-black text-white' : active==1, 'bg-gray-100 text-black' : active!==1 }"
+                         x-on:click="active=1">タブ2
+                    </div>
+                    <div class="flex-auto py-2 flex justify-center items-center"
+                         x-bind:class="{ 'bg-black text-white' : active==2, 'bg-gray-100 text-black' : active!==2 }"
+                         x-on:click="active=2">タブ3
+                    </div>
+                    <div class="flex-auto py-2 flex justify-center items-center"
+                         x-bind:class="{ 'bg-black text-white' : active==3, 'bg-gray-100 text-black' : active!==3 }"
+                         x-on:click="active=3">タブ4
+                    </div>
                 </div>
-                <div class="flex-auto py-2 flex justify-center items-center"
-                     x-bind:class="{ 'bg-black text-white' : active==1, 'bg-gray-100 text-black' : active!==1 }"
-                     x-on:click="active=1">タブ2
-                </div>
-                <div class="flex-auto py-2 flex justify-center items-center"
-                     x-bind:class="{ 'bg-black text-white' : active==2, 'bg-gray-100 text-black' : active!==2 }"
-                     x-on:click="active=2">タブ3
-                </div>
-                <div class="flex-auto py-2 flex justify-center items-center"
-                     x-bind:class="{ 'bg-black text-white' : active==3, 'bg-gray-100 text-black' : active!==3 }"
-                     x-on:click="active=3">タブ4
-                </div>
+                <ol>
+                    <li x-show="active==0">
+                        <div class="w-full min-h-[200px] bg-gray-100 p-8">
+                            <h3 class="text-lg font-bold">■コンテンツA</h3>
+                            <p class="text-base leading-relaxed">
+                                ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。
+                            </p>
+                        </div>
+                    </li>
+                    <li x-show="active==1">
+                        <div class="w-full min-h-[200px] bg-gray-100 p-8">
+                            <h3 class="text-lg font-bold">■コンテンツB</h3>
+                            <p class="text-base leading-relaxed">
+                                ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。
+                            </p>
+                        </div>
+                    </li>
+                    <li x-show="active==2">
+                        <div class="w-full min-h-[200px] bg-gray-100 p-8">
+                            <h3 class="text-lg font-bold">■コンテンツC</h3>
+                            <p class="text-base leading-relaxed">
+                                ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。
+                            </p>
+                        </div>
+                    </li>
+                    <li x-show="active==3">
+                        <div class="w-full min-h-[200px] bg-gray-100 p-8">
+                            <h3 class="text-lg font-bold">■コンテンツD</h3>
+                            <p class="text-base leading-relaxed">
+                                ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。ここには文章が入ります。
+                            </p>
+                        </div>
+                    </li>
+                </ol>
             </div>
         </section>
     </section>
